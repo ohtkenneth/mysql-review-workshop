95c261bd3a0a03876b05f76f5223b5902e02bd5a
David Engelstad <davidengelstad@gmail.com>
2018-08-27 18:56:05 -0700

    Exercise-2

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('todo_list', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+connection
+  .authenticate()
+  .then(() => console.log('Successfully connected to the database!'))
+  .catch(err =>
+    console.log('Error establishing connection to the database!', err)
+  );
+
+module.exports = connection;
